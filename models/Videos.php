684a6583c95e55942e0275224f48842cefeef669
David Persson <davidpersson@gmx.de>
2015-06-15 14:21:38 +0200

    Use Sluggable behavior instead of trait.

--- models/Videos.php
+++ models/Videos.php
@@ -16,8 +16,6 @@ use lithium\g11n\Message;
 
 class Videos extends \base_core\models\Base {
 
-	use \base_core\models\SlugTrait;
-
 	public $belongsTo = [
 		'VideoMedia' => [
 			'to' => 'base_media\models\Media',
@@ -26,6 +24,7 @@ class Videos extends \base_core\models\Base {
 	];
 
 	protected static $_actsAs = [
+		'base_media\extensions\data\behavior\Sluggable',
 		'base_media\extensions\data\behavior\Coupler' => [
 			'bindings' => [
 				'cover' => [
